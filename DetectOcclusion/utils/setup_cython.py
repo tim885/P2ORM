fef1c260924732066081e9fb89e02dc5422f6477
tim885 <xcqiutim@outlook.com>
2021-01-22 10:29:27 +0100

    update

--- DetectOcclusion/utils/setup_cython.py
+++ DetectOcclusion/utils/setup_cython.py
@@ -1,7 +1,10 @@
 # code for compile a cython .pyx file
 # usage: python setup_cython.py build_ext --inplace
 
+import numpy
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 
-setup(name='edge nms app', ext_modules=cythonize("edge_nms.pyx"))
+extensions = [Extension("edge_nms", ["edge_nms.pyx"])]
+setup(ext_modules=cythonize(extensions), include_dirs=[numpy.get_include()])
